cb4076143f35f19806c309108cf58a4655c4447f
Cliff Syner <c4syner@gmail.com>
2024-09-28 01:55:51 +0000

    fix: change host

--- src/main.rs
+++ src/main.rs
@@ -111,7 +111,7 @@ async fn main() {
         )
         .with_state(state);
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:4000")
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:4000")
         .await
         .unwrap();
     tracing::debug!("Reservation Paymaster API on http://{}", listener.local_addr().unwrap());
